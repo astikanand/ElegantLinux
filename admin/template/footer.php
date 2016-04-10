724379ca62c6f839aa298516f880be6cdf7d34ef
astikanand <astikanand@gmail.com>
2016-04-10 09:21:31 +0530

    admin side of website added

---
+++ admin/template/footer.php
@@ -0,0 +1,12 @@
+</div><!--Wrap Ends here -->
+	  
+	  <footer id="footer">
+         <p>Place sticky footer content here.</p>
+      </footer><!--Footer ends here-->
+	  
+	  <!-- Debug window -->
+	  <?php if($debug == 1) { include('widgets/debug.php'); }?>
+  	
+  </body>
+  
+</html>
