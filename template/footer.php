fdd41616c5d32f295dc6e37fba34a0b4e7217d85
astikanand <astikanand@gmail.com>
2016-04-10 09:30:07 +0530

    template directory and all the files inside it  added

---
+++ template/footer.php
@@ -0,0 +1,53 @@
+</div><!-- Wrap Ends -->
+           
+	 <div id="footer1" position="fixed">
+			<div id="section1">
+				<h3 style="margin-left:43px; color: white;text-align: left">Follow Us On</h3>
+				<ul >
+		             <li><a class="active" href="#home"><i class="fa fa-facebook-square "></i> Facebook</a></li>
+		             <li><a class="active" href="#home"><i class="fa fa-twitter "></i> Twitter</a></li>
+		             <li><a class="active" href="#home"><i class="fa fa-youtube "></i> Youtube</a></li>
+		             <li><a class="active" href="#home"><i class="fa fa-pinterest "></i> Pinterest</a></li>
+		             <li><a class="active" href="#home"><i class="fa fa-google-plus"></i> Google+</a></li>
+		             <li><a class="active" href="#home"><i class="fa fa-instagram "></i> Instagram</a></li>
+			         <li><a class="active" href="#home"><i class="fa fa-linkedin-square "></i> LinkedIn</a></li>
+		             
+			    </ul>
+				
+			</div>
+			<div id="section2">
+				<h3 style="margin-left: 65px" >About Us</h3>
+				<small><p >We are a young, creative and passionate team of thinkers, 
+				tech-maniacs,earthly designers,and rebellious educationists 
+				working together to cause turmoil in use of Linux as main OS
+				and to tell about some extra ordinary benefits of Linux OS.
+				We are committed to our goals, which we have set for the society 
+				and for us because we believe that do cool things that matter.</p>
+				</small>
+			</div>
+			<div id="section3">
+				<h3 style="margin-left: 55px">Contact Us</h3>
+				<form >
+		    		<input type="text" style="border-bottom: 2px solid blue" name="personName" placeholder="Your Name..." />
+		    		<input type="tel" style="border-bottom: 2px solid blue" name="personPhone" placeholder="Your Phone..."/>
+		    		<input type="email" style="border-bottom: 2px solid blue" name="personEmail" placeholder="Your email..." />
+		    		<input type="text" style="border-bottom: 2px solid blue" id="message" name="personMessage" placeholder="Write Your Message Here..." />
+		    		<input type="submit" value="Send Message"/>
+		    	</form>
+				
+			</div>
+			
+		</div>
+		<!-- footer2 -->
+		<div id="footer2">
+			<small style="margin-left: 530px">ElegantLinux © All Rights Reserved.</small>
+		</div>
+
+		
+		
+	  <!-- Debug window -->
+	  <?php if($debug == 1) { include('widgets/debug.php'); }?> 
+  	
+  </body>
+  
+</html>
